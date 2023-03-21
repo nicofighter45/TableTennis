6d764b92040facbf5b78f365bdaed86a59313dc7
nicofighter45 <fagot.nicolas.45@gmail.com>
2023-03-21 21:22:24 +0100

    data integration

--- main.cpp
+++ main.cpp
@@ -1,21 +1,59 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 string const error("An error occured, we're deeply sorry :(");
+string const textPath("C:/Users/fagot/OneDrive/Documents/MPSI/TIPE/data/en.txt");
+vector<string> paragraphs;
 
-int main()
+void openFile()
 {
-    ifstream textFile("data/en.txt");
+    ifstream textFile(textPath);
     if (!textFile)
         cerr << error << endl;
     string line;
+    string currentParagraph("");
+    int space = 0;
     while (getline(textFile, line))
     {
-        cout << line << endl;
+        if (space == 2)
+        {
+            space == 0;
+            paragraphs.push_back(currentParagraph);
+            currentParagraph = "";
+        }
+        else if (line == "")
+        {
+            space++;
+            if (space == 2)
+            {
+                continue;
+            }
+        }
+        currentParagraph += "\n" + line;
     }
+    paragraphs.push_back(currentParagraph);
     textFile.close();
+}
+
+string replace(string str, string from, string to)
+{
+    size_t start_pos = str.find(from);
+    if (start_pos == string::npos)
+        cerr << error << endl;
+    str.replace(start_pos, from.length(), to);
+    return str;
+}
+
+int main()
+{
+    openFile();
+    cout << paragraphs[0] << endl;
+    int simulationType;
+    cin >> simulationType;
+    cout << replace(paragraphs[1], "{type}", to_string(simulationType)) << endl;
     return 0;
 }
--- tools/constants.cpp
+++ tools/constants.cpp
@@ -6,5 +6,5 @@ double const gravity(9.81);
 double const spacing_per_second(1000);
 
 double const pi(3.141592653589793);
-double const surface(diametre_in_cm * diametre_in_cm * 0.0001 * pi * 2 / 3);
+double const surface(diametre_in_cm *diametre_in_cm * 0.0001 * pi * 2 / 3);
 double const mass(mass_in_gram * 0.001);
