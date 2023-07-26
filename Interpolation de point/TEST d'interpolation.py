afe6f709f112beed51e58e04649dad0ea6ed4c32
Nesnesty <Antoine.cousin@icloud.com>
2023-07-26 21:03:42 +0200

    yasss

--- Interpolation de point/TEST d'interpolation.py
+++ Interpolation de point/TEST d'interpolation.py
@@ -25,36 +25,46 @@ class matrice() :
         self.largeur = len(listedeliste)
     def __getitem__(self, indices):
         i, j = indices
-        return self.donnees[i][j]
+        return self.donnee[i][j]
     def __setitem__(self, indices, valeur):
         i, j = indices
-        self.donnees[i][j] = valeur
+        self.donnee[i][j] = valeur
     def __repr__(self):
-        return repr(self.donnees)
+        return repr(self.donnee)
     def __add__(A,B):
         if A.longueur == B.longueur and A.largeur == B.largeur :
             return(matrice([[A[i][j] + B[i][j] for j in range (len(A[i]))] for i in range (len(A))]))
         else : return(False)
-    def __mul__(A,B) :
-        if isinstance(B,matrice) :
-            n, m, l = A.largeur, A.longueur, B.longueur
-            if m != B.largeur :
-                raise Exception("The number of column of M1 must be the same as the number of line of M2")
-            M = []
+    def __mul__(self, B):
+        if isinstance(B, matrice):
+            n, m, l = self.largeur, self.longueur, B.longueur
+            if m != B.largeur:
+                raise Exception("The number of columns of M1 must be the same as the number of rows of M2")
+            
+            result = matrice([[0 for _ in range(l)] for _ in range(n)])
+            
             for i in range(n):
-                line = []
                 for j in range(l):
-                    sum = 0
                     for k in range(m):
-                        sum += A[i][k]*B[k][j]
-                    line.append(sum)
-                M.append(line)
-            return matrice(M)
-        if isinstance(B,int) :
-            for i in range (A.largeur) :
-                for j in range (A.longueur) :
-                    A[i][j]=A[i][j]*B
-            return(A)
+                        result[i, j] += self[i, k] * B[k, j]
+            
+            return result
+        elif isinstance(B, int):
+            result = matrice([[self[i, j] * B for j in range(self.longueur)] for i in range(self.largeur)])
+            return result
+    def __rmul__(self,B):
+        return self.__mul__(B)
+    def coefligne(self,alpha,ligne) :
+        for k in range (self.largeur) : 
+            if  1>=self[ligne,k]*alpha > 0.99 : self[ligne,k] = 1
+            elif 0.01>abs(self[ligne,k]*alpha) >= 0 : self[ligne,k] = 0
+            else : self[ligne,k]*=alpha 
+        return self
+    def liligne(self,ligne1,ligne2,alpha) :
+        for k in range (self.largeur) : self[ligne1,k]-= alpha*self[ligne2,k]
+        return self
+    def échange(self,ligne1,ligne2) :
+        for k in range (self.largeur) : self[ligne1,k],self[ligne2,k] = self[ligne2,k],self[ligne1,k]
 class polynome():
     def __init__(self, coefficient) :
         self.coefficient = coefficient
@@ -155,7 +165,7 @@ class ind_poly():
                 self.polynome=self.polynome * a
                 return self
             else : raise TypeError("Opération de multiplication non prise en charge.")
-    def __rmull__(self,a):
+    def __rmul__(self,a):
         return self.__mul__(a)
     def __str__(self) :
         return(f" { str(self.polynome) } * {str(self.indicatrice)} ")
@@ -209,31 +219,80 @@ def ind_degre(rang, degre):
     if degre == 0:
         return indicatrice(rang * intervalle, (rang + 1) * intervalle)
     else:
-        return (1 / ((degre) * intervalle)) * (
-            polynome([-rang * intervalle, 1]) * ind_degre(rang, degre - 1)
-            + polynome([(1 + rang + degre)* intervalle, -1]) * ind_degre(rang + 1, degre - 1)
+        return (1/(degre*intervalle)*
+            (polynome([-rang * intervalle, 1]) * ind_degre(rang, degre - 1)
+            + polynome([(1 + rang + degre)* intervalle, -1]) * ind_degre(rang + 1, degre - 1))
         )
 def approximation(liste, degre):
     fonction = Sum_ind_poly([])
-    for rang in range(len(liste) - degre):
-        L = ind_degre(rang, degre) * liste[rang]
+
+    for rang in range(len(liste)):
+        L = ind_degre(rang+degre+2, degre) * liste[rang]
         fonction = fonction + L
     return fonction
 
+def inverse(self):
+    if self.largeur != self.longueur:
+        raise ValueError("La matrice doit être carrée pour être inversible.")
+    n = self.largeur
+    Id = matrice([[1 if i == j else 0 for j in range(n)] for i in range(n)])  # Copie de la matrice identité
+    matrice_temp = matrice([ligne[:] for ligne in self.donnee])
 
+    for k in range(n):
+        if matrice_temp[k, k] == 0:
+            # Recherche d'une ligne non nulle pour effectuer un échange
+            for i in range(k+1, n):
+                if matrice_temp[i, k] != 0:
+                    # Échange de la ligne k avec la ligne i
+                    matrice_temp.donnee[k], matrice_temp.donnee[i] = matrice_temp.donnee[i], matrice_temp.donnee[k]
+                    Id.donnee[k], Id.donnee[i] = Id.donnee[i], Id.donnee[k]
+                    break
+            else:
+                raise ValueError("La matrice est singulière et n'est pas inversible.")
+
+        pivot_inv = 1 / matrice_temp[k, k]
+        matrice_temp.coefligne(pivot_inv, k)
+        Id.coefligne(pivot_inv, k)
+
+        for j in range(n):
+            if j != k:
+                alpha = matrice_temp[j, k]
+                matrice_temp.liligne(j, k, alpha)
+                Id.liligne(j, k, alpha)
+    print(self*Id)
+    return Id
+def fact (n):
+    if n==1 : return 1
+    else : return fact(n-1)*n
 def interpolation(liste,degre) :
     fonction = Sum_ind_poly([])
-    mat = matrice([[0 for k in range (len(liste))]*len(liste)])
-    liste =[]
-    for rang in range (len(liste)-degre) :
+    mat1 = matrice([[0]]+[[k]for k in liste]+[[0]])
+    mat2 = matrice([[0 for k in range (len(liste)+2)]for k in range(len(liste)+2)])
+    liste1 =[]
+    m=len(liste)
+    for rang in range (m+2) :
         L = ind_degre(rang,degre)
-        liste.append(L)
+        liste1.append(L)
+    for k in range (m) :
+        for i in range (m+2) : mat2[k+1,i] = liste1[i]((k+3)*intervalle)
+    liste1der = [der(l) for l in liste1]
+    for j in range (m+2) : 
+        mat2[0,j] = liste1der[j](5*intervalle)
+        mat2[-1,j] = liste1der[j](m*intervalle)
+    inverse1 = résolution(mat2,mat1)
+    point_de_controle = inverse1*mat1
+    for k in range (0,m+2): fonction = fonction + liste1[k]*point_de_controle[k,0]
+    return fonction
 
 
-import matplotlib.pyplot as plt
+def der(self) :
+        if type(self) == ind_poly : return ind_poly(self.indicatrice,der(self.polynome))
+        elif type(self) == polynome : return polynome([k*self.coefficient[k] for k in range (1,self.degre+1)])
+        elif type(self) == Sum_ind_poly : return Sum_ind_poly([der(k) for k in self.liste])
+        else : raise Exception("Pas dérivable")
 
 # Les points d'interpolation 
-y_interpolation = [rd.uniform(-1,1) for k in range(10)]
+y_interpolation = [ rd.uniform(k,k+1)  for k in range(10,41)]
 
 # Convertir les points d'interpolation en listes de coordonnées x et y
 x_interpolation = [ k for k in range (len(y_interpolation))]
@@ -245,7 +304,7 @@ degre = 3
 fonction_interpolation = approximation(y_interpolation, degre)
 
 # Calculer les valeurs interpolées pour l'affichage
-L = [fonction_interpolation(k/100) for k in range(1000)]
+L = [fonction_interpolation((3+k/100)*intervalle) for k in range(4100)]
 
 # Afficher les points d'interpolation
 plt.scatter(x_interpolation, y_interpolation, color='red', label='Points d\'interpolation')
@@ -259,4 +318,3 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title(f'Interpolation B-spline de degré {degre}')
 plt.show()
-
