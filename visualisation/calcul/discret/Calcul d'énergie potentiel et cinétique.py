5c8d644389e8627f0440b2011cde606ac59bfbf4
Nesnesty <Antoine.cousin@icloud.com>
2023-12-14 14:52:36 +0100

    voici

--- visualisation/calcul/discret/Calcul d'énergie potentiel et cinétique.py
+++ visualisation/calcul/discret/Calcul d'énergie potentiel et cinétique.py
@@ -12,17 +12,22 @@ def courbe(h,nombre):
     convertisseur(h, Taille_pix)
     t = [k * 10e-3 for k in range(len(h))]
     potentiel = np.array(energie_potentielle(h))
+    max = localmaxexperimentale(h)
+    max1,max2 = [max[k][0]for k in range (len(max))],[max[k][1]*10e-3 for k in range (len(max))]
+    max1 = energie_potentielle(max1)
     cinetique = np.array([0] + energie_cinétique1(h))
     Mecanique = potentiel + cinetique
     plt.plot(t, potentiel, label="Énergie potentiel")
     plt.plot(t, cinetique, label="Énergie cinétique")
     plt.plot(t, Mecanique, label="Énergie mécanique")
     plt.title("Évolution de l'énergie au cours du temps / courbe {}".format(nombre))
+    plt.scatter(max2,max1 , color='red', label='Points d\'interpolation')
     plt.ylabel("Énergie en Joule")
     plt.xlabel("Temps en seconde")
     plt.legend()
     plt.show()
     plt.clf()
+
 for k in range (15) :
     Taille_pix, h, x = data("TableTennis/output/4-jets-de-balle/tracked-{}.txt".format(k))
     courbe(x,k)
--- visualisation/tools/data_reception.py
+++ visualisation/tools/data_reception.py
@@ -1,3 +1,4 @@
+import collections as c
 Taille_pix =  137.1e-2/911
 def data(texte):
     with open(texte, "r") as file:
@@ -13,10 +14,18 @@ def data(texte):
 
 def premièrevaleurhaute(liste):
     p = 0
-    for k in range(len(liste)//3):
+    for k in range(len(liste)//2):
         if liste[k] <= liste[k + 1]:
             p = k
     return liste[p + 1:]
+
+def maxindice(x):
+    if len(x) == 0  : return (x)
+    if len(x) == 1  : return (x[0],0)
+    max,indice = x[0],0
+    for l in range (len(x)):
+        if max < x[l]: max,indice = x[l],l
+    return (max,indice)
 
 
 def maximum(liste):
@@ -37,3 +46,49 @@ def maximum(liste):
             p = -1
             k += 1
     return max
+
+def maxsubdivition(liste,i,j) :
+    liste2 = liste[i:j]
+    n = maxindice(liste2)
+    if n != [] : return n[1] + i
+    else :return(i)
+
+def localmax(liste) :
+    listmax =[(liste[0],0)]
+    for k in range (1,len(liste)-1) :
+        if liste[k-1] <= liste[k]>= liste[k+1] :
+            listmax.append((liste[k],k))
+    return(listmax)
+
+def localmaxexperimentale(liste) :
+    max = localmax(liste)
+    if len(max) ==1 : return([])
+    max1= [max[k][0] for k in range (len(max))]
+    n = len(max)
+    listesubdivision = c.deque([(0,0)])
+    maxi = max1[0]
+    for k in range (n) :
+        if abs(maxi-max1[k]) > maxi*0.25 :
+            listesubdivision.append((listesubdivision[-1][1]+1,k-1))
+            maxi = max1[k]
+    listesubdivision.append((listesubdivision[-1][1]+1,n-1))
+    listesubdivision.popleft()
+    indice = [ maxsubdivition(max1,k[0],k[1]) for k in listesubdivision]
+    return([max[k] for k in indice])
+
+def bond_division(liste):
+    subdivision=[]
+    min = 0
+    for k in range (1,len(liste)-1) :
+        if liste[k] < liste[k+1] and liste[k] < liste[k-1] :
+            subdivision.append((min,k))
+            min = k+1
+    return(subdivision)
+
+def haut_du_bond(liste):
+    liste1=[]
+    sub = bond_division(liste)
+    for k in sub :
+        (max,indice) = maxindice(liste[k[0]:k[1]])
+        liste1.append((max,indice+k[0]))
+    return(liste1)
